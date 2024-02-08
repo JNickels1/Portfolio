d63b1c13bafe289e5ccc749095d230d6b848617f
JNickels1 <nickelsjaime@gmail.com>
2024-02-08 17:16:42 -0500

    test commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Jaime Nickels</h1>
+      <h3>Software Engineer</h3>
+      <div className="card">
+        
+      </div>
+      
+    </>
+  )
+}
+
+export default App
