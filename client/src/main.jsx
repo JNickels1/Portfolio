da844a311833aff44d442a6fce5a9636044d346d
JNickels1 <nickelsjaime@gmail.COM>
2024-07-01 14:25:51 -0500

    trying to fix deployment issue ("MIME" type)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import emailjs from '@emailjs/browser';
+
+emailjs.init('E30t3DObnkIRuhpjI');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/portfolio/', // Set the base path to the repository name
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
   build: {
     outDir: 'dist', // Ensure the output directory is correctly set
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
   },
 });
