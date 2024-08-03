c9720753718d66529e55d99082b830ffbcfb37a9
JNickels1 <nickelsjaime@gmail.com>
2024-08-03 11:57:08 -0400

    edited vite settings

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,4 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-  },
-  build: {
-    outDir: 'dist', // Ensure the output directory is correctly set
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
-  },
 });
